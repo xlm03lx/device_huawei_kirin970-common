40eba4b7bf522249e1eb0c97663fa0b3de718ae4
xlm03lx <xlm03lx@gmail.com>
2020-06-21 05:53:43 +0800

    add some patches

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -2,7 +2,7 @@ echo $1
 rootdir="$PWD"
 # ---------------------------------
 
-dirs="build external/selinux frameworks/av frameworks/base frameworks/native frameworks/opt/net/wifi frameworks/opt/telephony packages/services/Telephony system/bt system/core system/netd system/sepolicy system/vold"
+dirs="build external/selinux frameworks/av frameworks/base frameworks/native frameworks/opt/net/wifi frameworks/opt/telephony packages/apps/Settings packages/services/Telephony system/bt system/core system/netd system/sepolicy system/vold"
 
 for dir in $dirs ; do
 	cd $rootdir
